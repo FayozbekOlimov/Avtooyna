af5607c7b663dcc3499b3a174928fd64e8a7687a
Abdumajidov-Bobojon <abdumajidovbobojon9@gmail.com>
2022-05-13 10:00:58 +0500

    certificate carusel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './static/variables.css';
-import './static/base.css';
 import Main from './pages/Main';
+import Index from "./pages/About/Certificates/index";
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './static/theme';
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import './static/variables.css';
+import './static/base.css';
 
 const App = () => {
 	return (
 		<ThemeProvider theme={theme}>
-			<Main />
+			{/* <Main /> */}
+			<Index/>
 		</ThemeProvider>
 	);
 }
