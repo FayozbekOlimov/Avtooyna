c8f649609da3ae475f499c24c25338006a56d1e7
ravshanbe <faxridinkf@gmail.com>
2022-05-14 08:14:22 +0500

    refactor: footer done

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,36 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+
 import "./_style.scss";
 import FooterLeftBlock from "./LeftBlock";
+import RightBlock from "./RightBlock";
 
 export default function Footer() {
   return (
-    <div className="footer_wrapper">
-      <div className="container">
-        <FooterLeftBlock />
-      </div>
-      <div className="footer">
+    <>
+      <div className="footer_wrapper">
         <div className="container">
-     <div className='footer_flex_box'>
-     <img src="/assets/img/logo.png" alt="" />
-          <Link to={"#"} className='copyright'>www.avtooyna.uz 2022</Link>
-          <div className="socila_link">
-            <a href="" alt="">
-              <img src="/assets/icon/fb.svg" alt="" />
-            </a>
-            <a href="" alt="">
-              <img src="/assets/icon/youtube.svg" alt="" />
-            </a>
-            <a href="" alt="">
-              <img src="/assets/icon/tg.svg" alt="" />
-            </a>
-            <a href="" alt="">
-              <img src="/assets/icon/insta.svg" alt="" />
-            </a>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 1, md: 2 }}
+          >
+            <Grid item xs={6}>
+              <FooterLeftBlock />
+            </Grid>
+            <Grid item xs={6}>
+              <RightBlock />
+            </Grid>
+          </Grid>
+        </div>
+        <div className="footer">
+          <div className="container">
+            <div className="footer_flex_box">
+              <img src="/assets/img/logo.png" alt="" />
+              <Link to={"#"} className="copyright">
+                www.avtooyna.uz 2022
+              </Link>
+              <div className="socila_link">
+                <a href="" alt="">
+                  <img src="/assets/icon/fb.svg" alt="" />
+                </a>
+                <a href="" alt="">
+                  <img src="/assets/icon/youtube.svg" alt="" />
+                </a>
+                <a href="" alt="">
+                  <img src="/assets/icon/tg.svg" alt="" />
+                </a>
+                <a href="" alt="">
+                  <img
+                    src="/assets/icon/insta.svg"
+                    alt=""
+                    id="footer_lastchild"
+                  />
+                </a>
+              </div>
+            </div>
           </div>
-     </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import About from '../../components/About'
-import Advantages from '../../components/Advantages'
-import Banner from '../../components/Banner'
-import Gallery from '../../components/Gallery'
-import News from '../../components/News'
-import Footer from '../../layout/Footer'
-import Header from '../../layout/Header'
+import React from "react";
+// import About from "../About";
+// import Advantages from "../";
+// import Banner from "../../pages/Banner";
+// import Gallery from "../../pages/Gallery";
+// import News from "../../pages/News";
+import Footer from "../../layout/Footer";
+import Header from "../../layout/Header";
 
 const Main = () => {
-    return (
-        <>
-            <Header />
-            <Banner />
-            <Advantages />
-            <About />
-            <News />
-            <Gallery />
-            <Footer/>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      {/* <Banner /> */}
+      {/* <Advantages /> */}
+      {/* <About /> */}
+      {/* <News />
+      <Gallery /> */}
+      <Footer />
+    </>
+  );
+};
 
-export default Main
+export default Main;
