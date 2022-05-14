cd9990fcf760d02b5829c87d366a40c23f111523
ravshanbe <faxridinkf@gmail.com>
2022-05-14 08:15:33 +0500

    ...

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import About from "../About";
-// import Advantages from "../";
-// import Banner from "../../pages/Banner";
-// import Gallery from "../../pages/Gallery";
-// import News from "../../pages/News";
+import About from "../About";
+import Advantages from "../";
+import Banner from "../../pages/Banner";
+import Gallery from "../../pages/Gallery";
+import News from "../../pages/News";
 import Footer from "../../layout/Footer";
 import Header from "../../layout/Header";
 
@@ -11,11 +11,11 @@ const Main = () => {
   return (
     <>
       <Header />
-      {/* <Banner /> */}
-      {/* <Advantages /> */}
-      {/* <About /> */}
-      {/* <News />
-      <Gallery /> */}
+      <Banner />
+      <Advantages />
+      <About />
+      <News />
+      <Gallery />
       <Footer />
     </>
   );
