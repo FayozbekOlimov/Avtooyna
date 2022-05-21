80c7aff20e42abedb0fd227eee9226fd223d5593
fakhriddin <faxridinkf@gmail.com>
2022-05-21 11:39:10 +0500

    home banner...

--- src/pages/Main/Banner/index.js
+++ src/pages/Main/Banner/index.js
@@ -3,18 +3,19 @@ import Button from '../../../components/Button'
 import Title from '../../../components/Title'
 import Text from '../../../components/Text';
 import { homeBannerUrl } from "../../../api/apiUrls";
+import{ API_IMG_URL} from '../../../constants/index'
 import baseAPI from "../../../api/baseAPI";
 import './style.scss';
 
 
 const Banner = () => {
-    const [banner, setBanner] = useState();
+    const [banner, setBanner] = useState({});
 
     const getBanner = useCallback(() => {
         baseAPI.fetchAll(homeBannerUrl)
             .then(res => {
                 if (res.data.status === 200) {
-                    setBanner(res.data)
+                    setBanner(res.data.homeBanner)
                     console.log("ban", banner);
                 }
             })
@@ -32,15 +33,15 @@ const Banner = () => {
     return (
         <div className='banner'>
             <div className="banner__bg">
-                {/* <img src={imgs} alt="banner" /> */}
+                <img src={API_IMG_URL+banner.imgs} alt="banner" />
             </div>
             <div className="container">
                 <div className="banner__content">
                     <Title>
-                        {/* <div dangerouslySetInnerHTML={{ __html: title }}></div> */}
+                        {banner.title}
                     </Title>
                     <Text>
-                        {/* <div dangerouslySetInnerHTML={{ __html: text }}></div> */}
+                        {banner.text}
                     </Text>
                     <Button>Batafsil</Button>
                 </div>
